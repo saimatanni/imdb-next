ed69c25f76e1837c4ef95a7afd3cba966598b3e6
Shaima Tanni <tanni@devsstream.com>
2024-05-20 17:36:36 +0600

    fetch data from tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,21 @@
-import Image from "next/image";
+import Results from "@/components/Results";
 
-export default function Home() {
-  return (
-  <></>
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
   );
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(`Faild to fetch!`);
+  }
+  const results = data?.results;
+  console.log("result", results);
+  return <>
+  <Results results={results}/></>;
 }
